7e39e4055877d4185ad88b2627ce5c0499e1c0c3
Xin <wenxinxu0401@gmail.com>
2023-05-16 12:03:36 +0800

    24

---
+++ 24.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr){};
+    ListNode(int x) : val(x), next(nullptr){};
+    ListNode(int x, ListNode *next) : val(x), next(next){};
+};
+class Solution
+{
+public:
+    ListNode *swapPairs(ListNode *head)
+    {
+        if (head == nullptr || head->next == nullptr)
+        {
+            return head;
+        }
+        ListNode *nHead = swapPairs(head->next->next);
+        ListNode *right = head->next;
+        head->next = nHead;
+        right->next = head;
+        return right;
+    }
+};
+
+int main()
+{
+}
