248c18e1fcab35696018294bf80c70da9cd5ce26
Xin401 <wenxinxu0401@gmail.com>
2026-05-05 13:58:01 +0800

    cpp: 0061

--- problems/0061/solution.cpp
+++ problems/0061/solution.cpp
@@ -1,17 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int maxSum = nums[0];
-        int curr = 0;
-        for(int i = 0;i<nums.size();i++)
-        {
-            if(curr<0)
-            {
-                curr=0;
-            }
-            curr+=nums[i];
-            maxSum = max(maxSum,curr);
+    ListNode* rotateRight(ListNode* head, int k) {
+        if (head == nullptr) return nullptr;
+        vector<ListNode*> list;
+        ListNode * ptr = head;
+        while(ptr != nullptr) {
+            list.push_back(ptr);
+            ptr = ptr->next;
         }
-        return maxSum;
+        int m = list.size();
+        if (k == 0 || k % m == 0) return head;
+        k %= m;
+        list[m - 1]->next = list[0];
+        list[m - k - 1]->next = nullptr;
+        return list[m - k];
     }
 };
+
+int main() {
+    Solution sol;
+    ListNode * head = new ListNode(0, new ListNode(1, new ListNode(2)));
+    ListNode * newHead = sol.rotateRight(head, 4);
+    while (newHead != nullptr){
+        cout << newHead->val << ',' ;
+        newHead = newHead->next;
+    }
+}
