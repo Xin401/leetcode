b6fa91961097a61c2c31a29514792ec6148ce624
Xin401 <wenxinxu0401@gmail.com>
2026-05-05 14:02:54 +0800

    py: 0061

---
+++ problems/0061/solution.py
@@ -0,0 +1,21 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if (head == None): return None
+        array = []
+        ptr = head
+        while ptr != None:
+            array.append(ptr)
+            ptr = ptr.next
+        m = len(array)
+        if k == 0 or k % m == 0: return head;
+        k %= m;
+        array[m - 1].next = array[0]
+        array[m - k - 1].next = None
+        return array[m - k]
