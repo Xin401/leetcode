a532b470293a37b5c40b361e651767d1778105bc
Xin401 <wenxinxu0401@gmail.com>
2026-05-05 15:02:48 +0800

    format: 0061

--- problems/0061/solution.cpp
+++ problems/0061/solution.cpp
@@ -2,40 +2,39 @@
 #include <vector>
 using namespace std;
 
-
 struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
 
 class Solution {
-public:
-    ListNode* rotateRight(ListNode* head, int k) {
-        if (head == nullptr) return nullptr;
-        vector<ListNode*> list;
-        ListNode * ptr = head;
-        while(ptr != nullptr) {
-            list.push_back(ptr);
-            ptr = ptr->next;
-        }
-        int m = list.size();
-        if (k == 0 || k % m == 0) return head;
-        k %= m;
-        list[m - 1]->next = list[0];
-        list[m - k - 1]->next = nullptr;
-        return list[m - k];
+ public:
+  ListNode* rotateRight(ListNode* head, int k) {
+    if (head == nullptr) return nullptr;
+    vector<ListNode*> list;
+    ListNode* ptr = head;
+    while (ptr != nullptr) {
+      list.push_back(ptr);
+      ptr = ptr->next;
     }
+    int m = list.size();
+    if (k == 0 || k % m == 0) return head;
+    k %= m;
+    list[m - 1]->next = list[0];
+    list[m - k - 1]->next = nullptr;
+    return list[m - k];
+  }
 };
 
 int main() {
-    Solution sol;
-    ListNode * head = new ListNode(0, new ListNode(1, new ListNode(2)));
-    ListNode * newHead = sol.rotateRight(head, 4);
-    while (newHead != nullptr){
-        cout << newHead->val << ',' ;
-        newHead = newHead->next;
-    }
+  Solution sol;
+  ListNode* head = new ListNode(0, new ListNode(1, new ListNode(2)));
+  ListNode* newHead = sol.rotateRight(head, 4);
+  while (newHead != nullptr) {
+    cout << newHead->val << ',';
+    newHead = newHead->next;
+  }
 }
--- problems/0061/solution.py
+++ problems/0061/solution.py
@@ -1,20 +1,25 @@
 from typing import Optional
 
+
 # Definition for singly-linked list.
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        if (head == None): return None
+        if head == None:
+            return None
         array = []
         ptr = head
         while ptr != None:
             array.append(ptr)
             ptr = ptr.next
         m = len(array)
-        if k == 0 or k % m == 0: return head
+        if k == 0 or k % m == 0:
+            return head
         k %= m
         array[m - 1].next = array[0]
         array[m - k - 1].next = None
