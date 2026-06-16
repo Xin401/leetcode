868dfeb14cf2bd27c2c434427857787f6b27747e
WenXin Xu <wenxinxu@WenXins-MacBook-Air.local>
2026-06-16 14:47:37 +0800

    104

--- templates/02_linked_list/template.cpp
+++ templates/02_linked_list/template.cpp
@@ -5,7 +5,9 @@
 struct ListNode {
   int val;
   ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
   ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
 
 class LinkedListTemplates {
